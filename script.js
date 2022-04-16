63fa644d1524c7a7d5461de3bfa20a650d38eb12
Retrye <trkaly1@gmail.com>
2022-04-16 12:06:00 +0100

    Update all

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 alert("Heyy!!!");
+
+
+
+function changeVal(){
+    let inputValue = document.getElementById("numInput").value;
+
+    document.getElementById("meterInput").innerText = inputValue;
+    document.getElementById("feetOutput").innerText = (inputValue * 3.28084).toFixed(3);
+    document.getElementById("feetInput").innerText = inputValue;
+    document.getElementById("meterOutput").innerText = (inputValue / 3.28084).toFixed(3);
+
+    document.getElementById("literInput").innerText = inputValue;
+    document.getElementById("gallonOutput").innerText = (inputValue / 3.785).toFixed(3);
+    document.getElementById("gallonInput").innerText = inputValue;
+    document.getElementById("literOutput").innerText = (inputValue * 3.785).toFixed(3);
+
+    document.getElementById("kiloInput").innerText = inputValue;
+    document.getElementById("poundOutput").innerText = (inputValue * 2.20462).toFixed(3);
+    document.getElementById("poundInput").innerText = inputValue;
+    document.getElementById("kiloOutput").innerText = (inputValue / 2.20462).toFixed(3);
+    
+}
